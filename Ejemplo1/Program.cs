4d23b57514d18975c06f48f1c23974333cbeba00
unknown <hermetica411@gmail.com>
2024-10-07 19:48:09 -0300

    primera subida...

---
+++ Ejemplo1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //aqui .net5. diferencia entre .net5 y .net_fremework
+        }
+    }
+}
---
+++ Ejemplo2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mundo");
+            Console.ReadKey();
+            //aqui en freemwork lineas del 1 al 5
+        }
+    }
+}
